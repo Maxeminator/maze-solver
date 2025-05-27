6deea3efd967d8582d4d2237208fb9f74a8ddfc5
Maxeminator <leoniker@mail.ru>
2025-05-27 15:54:27 +0300

    Finished recursive wall breaking

--- graphics.py
+++ graphics.py
@@ -1,5 +1,7 @@
 from tkinter import Tk, BOTH, Canvas
 from time import sleep
+import random
+
 class Window:
     def __init__(self,width,height):
         self.width=width
@@ -47,6 +49,7 @@ class Cell:
         self.has_right_wall=True
         self.has_top_wall=True
         self.has_bottom_wall=True
+        self.visited=False
         self.__x1=-1
         self.__y1=-1
         self.__x2=-1
@@ -103,11 +106,14 @@ class Maze:
         self.cell_size_x=cell_size_x
         self.cell_size_y=cell_size_y
         self.win=win
+        if seed is not None:
+            random.seed(seed)
         self.seed=seed
         self.__cells=[]
         self.__create_cells()
         self.__break_entrance_and_exit()
-
+        self.__break_walls_r(0, 0)
+        
     def __create_cells(self):
         for i in range(self.num_cols):
             cols=[]
@@ -140,3 +146,42 @@ class Maze:
         self.__draw_cell(0,0)
         self.__cells[-1][-1].has_top_wall=False
         self.__draw_cell(len(self.__cells)-1,len(self.__cells[0])-1)
+
+    def __break_walls_r(self, i, j):
+        self.__cells[i][j].visited=True
+        while True:
+            next=[]
+            if j + 1 < self.num_rows and not self.__cells[i][j+1].visited:
+                next.append(("up",i,j+1))
+            if j-1>=0 and not self.__cells[i][j-1].visited:
+                next.append(("down",i,j-1))
+            if i - 1 >= 0 and not self.__cells[i - 1][j].visited:
+                next.append(("left", i - 1, j))
+            if i+1 < self.num_cols and not self.__cells[i+1][j].visited:
+                next.append(("right",i+1,j))
+            if len(next)==0:
+                self.__draw_cell(i,j)
+                return
+            else:
+                direction,ni,nj=random.choice(next)
+                match direction:
+                    case "up":
+                        self.__cells[i][j].has_top_wall=False
+                        self.__cells[ni][nj].has_bottom_wall=False
+                    case "right":
+                        self.__cells[i][j].has_right_wall=False
+                        self.__cells[ni][nj].has_left_wall=False
+                    case "left":
+                        self.__cells[i][j].has_left_wall=False
+                        self.__cells[ni][nj].has_right_wall=False
+                    case "down":
+                        self.__cells[i][j].has_bottom_wall=False
+                        self.__cells[ni][nj].has_top_wall=False
+                    case _:
+                        return
+                    
+                self.__draw_cell(i,j)
+                self.__draw_cell(ni,nj)
+                self.__break_walls_r(ni, nj)
+                return
+
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from graphics import Window, Maze
 def main():
 
     win=Window(800,600)
-    maze_test=Maze(10,10,10,10,10,10,win)
+    maze_test=Maze(10,10,10,10,10,10,win,0)
     win.wait_for_close()
     
 if __name__ == "__main__":
